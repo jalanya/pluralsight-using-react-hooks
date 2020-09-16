42b413600e1861c69bd1b41e720cc6e6a570aa7d
Jose Alanya <jose.alanya@gmail.com>
2020-09-15 21:04:31 -0500

    FEAT: Start Using React Hooks with useState, useEffect, and useRef: Improving and Fixing Our Scrolling Colorizing React App

--- src/ImageToggleOnScroll.js
+++ src/ImageToggleOnScroll.js
@@ -4,6 +4,8 @@ const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
     
     const imageRef = useRef(null);
 
+    const [isLoading, setIsLoading] = useState(true);
+
     const isInView = () => {
         const rect = imageRef.current.getBoundingClientRect();
         return rect.top >=0 && rect.bottom <= window.innerHeight;
@@ -12,6 +14,8 @@ const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
     const [inView, setInView] = useState(false);
 
     useEffect(() => {
+        setIsLoading(false);
+        setInView(isInView());
         window.addEventListener("scroll", scrollHandler);
         return () => {
             window.removeEventListener("scroll", scrollHandler);
@@ -23,9 +27,13 @@ const ImageToggleOnScroll = ({ primaryImg, secondaryImg }) => {
     };
 
     return (
-        <img src={inView? secondaryImg : primaryImg}
+        <img src={
+                isLoading ? 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==' // 1x1gif
+                    : inView ? secondaryImg : primaryImg}
             alt=""
             ref={imageRef}
+            width="200"
+            height="200"
         />
     );
 };
