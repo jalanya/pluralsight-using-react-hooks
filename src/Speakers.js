61344a9ff6bb34bfed5ffa5629396570648e1496
Jose Alanya <jose.alanya@gmail.com>
2020-10-06 17:25:42 -0500

    FEAT: Using More Hooks: useContext, useReducer, useCallback, and useMemo
    - Using useMemo to Optimize Filtering and Sorting Speakers

--- src/Speakers.js
+++ src/Speakers.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext, useReducer, useCallback } from 'react';
+import React, { useEffect, useState, useContext, useReducer, useCallback, useMemo } from 'react';
 
 import { Header } from '../src/Header';
 import { Menu } from '../src/Menu';
@@ -44,22 +44,24 @@ const Speakers = ({}) => {
     setSpeakingSaturday(!speakingSaturday);
   };
 
+  const newSpeakerList = useMemo(() => speakerList
+  .filter(
+    ({ sat, sun }) =>
+      (speakingSaturday && sat) || (speakingSunday && sun),
+  )
+  .sort(function (a, b) {
+    if (a.firstName < b.firstName) {
+      return -1;
+    }
+    if (a.firstName > b.firstName) {
+      return 1;
+    }
+    return 0;
+  }), [speakingSaturday, speakerList])
+
   const speakerListFiltered = isLoading
     ? []
-    : speakerList
-        .filter(
-          ({ sat, sun }) =>
-            (speakingSaturday && sat) || (speakingSunday && sun),
-        )
-        .sort(function (a, b) {
-          if (a.firstName < b.firstName) {
-            return -1;
-          }
-          if (a.firstName > b.firstName) {
-            return 1;
-          }
-          return 0;
-        });
+    : newSpeakerList;
 
   const handleChangeSunday = () => {
     setSpeakingSunday(!speakingSunday);
