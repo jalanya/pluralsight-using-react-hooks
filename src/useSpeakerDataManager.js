878197607611d84b4cb6b152000629e6285d2145
Jose Alanya <jose.alanya@gmail.com>
2020-10-13 17:22:02 -0500

    FEAT: Learn How to Combine Existing React Hooks into New Combined Hooks
    - Converting Our Custom React Hook to Use REST Instead of a Static Array

--- src/useSpeakerDataManager.js
+++ src/useSpeakerDataManager.js
@@ -1,7 +1,7 @@
 import { useEffect, useReducer } from 'react';
-
+import axios from 'axios';
 import { speakersReducer } from './speakersReducer';
-import SpeakerData from './SpeakerData';
+//import SpeakerData from './SpeakerData';
 
 function useSpeakerDataManager () {
   const [{ isLoading, speakerList }, dispatch] = useReducer(speakersReducer, {
@@ -10,23 +10,32 @@ function useSpeakerDataManager () {
   });
 
   function toggleSpeakerFavorite(speakerRec) {
-    speakerRec.favorite === true ?
-      dispatch({type: "unfavorite", id: speakerRec.id}) :
-      dispatch({type: "favorite", id: speakerRec.id});
+    const updateData = async function() {
+      axios.put(`http://localhost:4000/speakers/${speakerRec.id}`, speakerRec);
+      speakerRec.favorite === true ?
+        dispatch({type: "unfavorite", id: speakerRec.id}) :
+        dispatch({type: "favorite", id: speakerRec.id});
+    };
+    updateData();
   }
 
   useEffect(() => {
-    new Promise(function (resolve) {
-      setTimeout(function () {
-        resolve();
-      }, 1000);
-    }).then(() => {
-      //setSpeakerList(speakerListServerFilter);
-      dispatch({
-        type: "setSpeakerList",
-        data: SpeakerData
-      });
-    });
+    // new Promise(function (resolve) {
+    //   setTimeout(function () {
+    //     resolve();
+    //   }, 1000);
+    // }).then(() => {
+    //   //setSpeakerList(speakerListServerFilter);
+    //   dispatch({
+    //     type: "setSpeakerList",
+    //     data: SpeakerData
+    //   });
+    // });
+    const fetchData = async function() {
+      let result = await axios.get('http://localhost:4000/speakers');
+      dispatch({type: 'setSpeakerList', data: result.data})
+    }
+    fetchData();
     return () => {
       console.log('cleanup');
     };
